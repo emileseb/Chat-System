b4e5c8288aeae6c7948a7a870cb656921f3690eb
emileseb <emile.seb@gmail.com>
2019-12-02 16:18:59 +0100

    ready to pull

--- chat-system/src/Agent.java
+++ chat-system/src/Agent.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 
 public class Agent {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("coucou");
+        Utilisateur jj = new Utilisateur("JJ", new Id(), true);
+        Utilisateur thomas = new Utilisateur("Thomas", new Id(), true);
+        ArrayList<Message> msglist = new ArrayList<Message>();
+        System.out.println(thomas.getAdresseIp());
+
 	}
 
 }
