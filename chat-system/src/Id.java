54f1c0354765640eb6aa799f4e60dcba5715cfd9
emileseb <emile.seb@gmail.com>
2019-12-02 14:48:53 +0100

    Creation Id class

--- chat-system/src/Id.java
+++ chat-system/src/Id.java
@@ -1,7 +1,31 @@
-public class Id {
-    private int value;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
-    public Id(int value){
-        this.value = value;
+public class Id {
+    private long value;
+
+    public Id() {
+        try {
+            Enumeration<NetworkInterface> networkInterface = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface network = networkInterface.nextElement();
+            byte[] mac = network.getHardwareAddress();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "" : ""));
+            }
+            System.out.println();
+            this.value = Long.parseLong(sb.toString(), 16);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+    public long getValue () {
+        return this.value;
+    }
+
+
+    public boolean equals(Id id) {
+        return (this.value == id.value);
     }
 }
