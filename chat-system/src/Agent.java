dc2a46925e9fbb23f33e7db80c0bb126dc08fafe
M. Girard Louis <lgirard@insa-10229>
2019-12-02 16:17:57 +0100

    agent update

--- chat-system/src/Agent.java
+++ chat-system/src/Agent.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 
 public class Agent {
-
+	
+	//utilisateur
+	private ArrayList<Utilisateur> listeUtilisateurs;
+	
+	public boolean clientExiste(Id idAgent){
+		boolean result = false;
+		for (Utilisateur utilisateur : listeUtilisateurs) {
+			if (utilisateur.getId().equals(idAgent)) {
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	//historique
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("coucou");
+		Utilisateur agent1 = new Utilisateur("Emile", new Id(), true);
+		
+		System.out.println(agent1.getAdresseIp());
 	}
 
 }
