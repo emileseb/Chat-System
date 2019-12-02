5dffa24740260c64797931aadf3bb9a3ddc59beb
M. Girard Louis <lgirard@insa-10229>
2019-12-02 14:51:17 +0100

    Ajout classe message

---
+++ chat-system/src/Message.java
@@ -0,0 +1,36 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Message {
+
+	private Utilisateur auteur;
+	private Utilisateur destinataire;
+	private String contenu;
+	private Date date;
+	
+	public Message(Utilisateur auteur, Utilisateur destinataire, String contenu) {
+		this.auteur = auteur;
+		this.destinataire = destinataire;
+		this.contenu = contenu;
+		this.date = new Date();
+	}
+	
+	public Utilisateur getAuteur() {
+		return this.auteur;
+	}
+
+	public Utilisateur getDestinataire() {
+		return this.destinataire;
+	}
+	
+	public String getDate() {
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyy");
+		return format.format(this.date);
+	}
+	
+	public String toString() {
+		return ("Auteur - " + this.auteur.getPseudo() + ", Destinataire - " + this.destinataire.getPseudo() + ", Date - " +
+	getDate() + " : " + this.contenu);
+	}
+	
+}
