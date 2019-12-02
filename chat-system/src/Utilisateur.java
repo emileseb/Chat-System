8553e676ae3d9d7764e4a33be344a01236ddd1bf
M. Girard Louis <lgirard@insa-10229>
2019-12-02 15:05:13 +0100

    Modif classe utilisateur

--- chat-system/src/Utilisateur.java
+++ chat-system/src/Utilisateur.java
@@ -1,3 +1,47 @@
-public class Utilisateur {
+import java.net.*;
+import java.util.Enumeration;
 
+public class Utilisateur {
+	private String pseudo;	
+	private Id idUtilisateur;
+	private boolean actif;
+	private String adresseIp;
+	
+	public Utilisateur(String pseudo, Id idUtilisateur, boolean actif) {
+		this.pseudo = pseudo;
+		this.idUtilisateur = idUtilisateur;
+		this.actif = actif;
+		
+	    
+		try {
+			this.adresseIp = InetAddress.getLocalHost().getHostAddress();
+		}
+		catch (UnknownHostException e) {
+			this.adresseIp = "";
+		}
+	}
+	
+	public String getPseudo() {
+		return this.pseudo;
+	}
+	
+	public Id getId() {
+		return this.idUtilisateur;
+	}
+	
+	public String getAdresseIp() {
+		return this.adresseIp;
+	}
+	
+	public boolean getActif() {
+		return this.actif;
+	}
+	
+	public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+	
+	public void changePseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
 }
