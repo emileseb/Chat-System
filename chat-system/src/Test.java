c34ff99145aa8e16231c67e9e45beaeb44ebf2ec
M. Girard Louis <lgirard@insa-08133>
2020-01-08 10:20:09 +0100

    test tcp sur 2 ordi

--- chat-system/src/Test.java
+++ chat-system/src/Test.java
@@ -3,12 +3,10 @@ import utilisateur.*;
 
 public class Test {
     public static void main(String[] args) {
-		Utilisateur serverGuy = new Utilisateur("servguy", new Id(1111111111), "127.0.0.1");
-		Utilisateur client1 = new Utilisateur("Thomas", new Id(222222222), "127.0.0.1");
-		Utilisateur client2 = new Utilisateur("Théo", new Id(333333333), "127.0.0.1");
+		Utilisateur serverGuy = new Utilisateur("servguy", new Id(1), "10.1.5.16");
+		Utilisateur client1 = new Utilisateur("client", new Id(2), "10.1.5.99");
 		client1.getListeUtilisateurs().add(serverGuy);
-		client1.getListeUtilisateurs().add(client2);
 
-		ClavardageManager.demandeClavardage(client1, new Id(1111111111));
+		ClavardageManager.demandeClavardage(client1, serverGuy.getId());
 	}
 }
